469a074a185d86b30adaafc68875da3ea9207ecc
Thomas Liebhart <51077028+liebtho@users.noreply.github.com>
2024-09-13 12:58:21 +0200

    Update automaton.js

--- automaton.js
+++ automaton.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const canvas = document.querySelector('canvas');
+    const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
     function resizeCanvas() {
